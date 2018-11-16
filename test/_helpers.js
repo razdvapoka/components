a55e9c7c1fecc75d26f1fa120ac0d4ce0da8cb40
Ivan Grishin <r@exah.me>
2018-11-16 19:57:13 +0300

    🚧 Add serializer

--- test/_helpers.js
+++ test/_helpers.js
@@ -1,6 +1,22 @@
+import * as emotion from 'emotion'
 import renderer from 'react-test-renderer'
+import { createSerializer } from 'jest-emotion'
 
-const renderJSON = (element) => renderer.create(element).toJSON()
+const serializer = createSerializer(emotion)
+
+const renderJSON = (element) => {
+  const json = renderer.create(element).toJSON()
+
+  if (serializer.test(json)) {
+    const styles = serializer.print(json, (res) => '')
+    return [
+      styles.trim(),
+      json
+    ]
+  }
+
+  return json
+}
 
 export {
   renderJSON
